065251c95b01ae7da62ee40bf0202af9dc1643ac
Kiryl Fedaryshkin <kfedorishkin@mail.ru>
2021-10-27 14:43:57 +0200

    Prevent of crash because of missing directory

--- Linux/code/folder_stats.cpp
+++ Linux/code/folder_stats.cpp
@@ -18,33 +18,37 @@
 namespace fs = std::filesystem;
 
 int main(int argc, char **argv) {
+
     testing::InitGoogleTest(&argc, argv);
     RUN_ALL_TESTS();
 
-    
-    std::vector<long int> v;
-    auto root = fs::path("../test_cases");
-    std::unordered_map<std::string, bool> filter = {
-        {".txt", true},
-    };
-    auto folder_stat = getFilePathsElements(root, filter);
-    for (auto i = 1u; i < std::thread::hardware_concurrency()-1; i++) {
-        std::cout << "NUMBER OF FILES:" << folder_stat.first << std::endl;
-        std::cout << "[=================================================================]" << std::endl;
-        auto start = std::chrono::high_resolution_clock::now();
-        {
-            Thread_pool tp(i, folder_stat.second, &v);
-            std::cout << "Number of threads:" << tp.number_of_active_threads() << std::endl;
+    auto folder_for_analysis = fs::path("../tests_cases"); 
+    if (fs::exists(folder_for_analysis)) {
+        std::vector<long> v;
+        std::unordered_map<std::string, bool> filter = {
+            {".txt", true},
+        };
+        auto folder_stat = getFilePathsElements(folder_for_analysis, filter);
+        for (auto i = 1u; i < std::thread::hardware_concurrency() - 1; i++) {
+            std::cout << "NUMBER OF FILES:" << folder_stat.first << std::endl;
+            std::cout << "[=================================================================]" << std::endl;
+            auto start = std::chrono::high_resolution_clock::now();
+            {
+                Thread_pool tp(i, folder_stat.second, &v);
+                std::cout << "Number of threads:" << tp.number_of_active_threads() << std::endl;
+            }
+            auto end = std::chrono::high_resolution_clock::now();
+            print_res(v);
+            std::chrono::duration<float> duration = end - start;
+            std::cout << "Duration:" << duration.count() << 's' << std::endl;
+            std::cout << "[=================================================================]" << std::endl;
+            std::cout << std::endl;
+            std::cout << std::endl;
+            std::cout << std::endl;
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        print_res(v);
-        std::chrono::duration<float> duration = end - start;
-        std::cout << "Duration:" << duration.count() << 's' << std::endl;
-        std::cout << "[=================================================================]" << std::endl;
-        std::cout << std::endl;
-        std::cout << std::endl;
-        std::cout << std::endl;
     }
-
+    else {
+        std::cout << std::endl << "Error:No such folder" << std::endl;
+    }
     return 0;
 }
